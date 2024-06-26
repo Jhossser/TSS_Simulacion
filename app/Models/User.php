67f2f6f35df7bf777bd32b91e553e52bfd405a42
Jhosemar <jhoten703@gmail.com>
2024-06-26 12:53:51 -0400

    registro de usuario terminado

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'nombre',
         'password',
     ];
 
@@ -41,4 +40,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getNombreAttribute($value){
+        return ucwords($value);
+    }
+    public function setNombreAttribute($value){
+        $this->attributes['nombre'] = strtolower($value);
+    }
 }
